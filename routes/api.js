ab370b4b7b6df00ed6c0fe76ddfc7cb298bdf8be
Maxgit3 <52330733+Maxgit3@users.noreply.github.com>
2025-05-23 05:17:51 +0000

    initial commit

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,38 @@
 'use strict';
 
+const url = "https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/[symbol]/quote"
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async function (req, res){
+      // console.log(req.query)
+      if (req.query.stock) {
+        const stock = req.query.stock;
+        // console.log(stock);
+        const like = req.query.like;
+        // console.log(like);
+        const newURl = url.replace("[symbol]", stock);
+        console.log(newURl);
+        // app.get(newURl, (req, res) => {
+        //   const stockData = res.data;
+        //   console.log(stockData);
+        //   if (stockData) {
+        //     const response = {
+        //       stock: stock,
+        //       price: stockData.latestPrice,
+        //       likes: like ? 1 : 0
+        //     };
+        //     res.json(response);
+        //   } else {
+        //     res.status(404).send('Stock not found');
+        //   }
+        // });
+        // Fetch the stock data from the API
+        const stockresponse = await fetch(newURl)
+        const stockData = await stockresponse.json();
+        console.log(stockData);
+        return res.json({"stockData": {}})
+      }
     });
     
 };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
+const helmet = require('helmet');
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -17,6 +18,15 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Use helmet to secure Express headers
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    styleSrc: ["'self'"],
+    scriptSrc: ["'self'"]
+  }
+}));
+// app.use(helmet.xssFilter());
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
